8f1be99841fc5c06341466816007a1257f5ccf28
Scott W Harden <swharden@gmail.com>
2021-06-23 20:24:26 -0400

    log exceptions

--- src/AbfDB/Program.cs
+++ src/AbfDB/Program.cs
@@ -25,6 +25,10 @@ namespace AbfDB
             int count = 0;
             Stopwatch sw = Stopwatch.StartNew();
 
+            if (File.Exists("log.txt"))
+                File.Delete("log.txt");
+            using StreamWriter log = File.AppendText("log.txt");
+
             foreach (string path in Directory.EnumerateFiles(args[0], "*.abf", SearchOption.AllDirectories))
             {
                 if (path.EndsWith(".abf") == false)
@@ -32,19 +36,35 @@ namespace AbfDB
 
                 string fullPath = Path.GetFullPath(path);
                 Console.WriteLine($"ABFs={count++} Elapsed={sw.Elapsed} Path={fullPath}");
-                AbfSharp.ABFFIO.ABF abf = new(fullPath);
 
-                foreach (AbfDatabase database in databases)
+                try
                 {
-                    database.AddAbf(
-                        path: fullPath,
-                        episodes: abf.Header.lActualEpisodes,
-                        date: abf.Header.uFileStartDate,
-                        time: abf.Header.uFileStartTimeMS,
-                        stopwatch: abf.Header.lStopwatchTime
-                    );
+                    // Use ABFFIO.DLL - benchmark scan of 628 ABFs (over network) took 18.04 seconds
+                    AbfSharp.ABFFIO.ABF abf = new(fullPath);
+
+                    // Use native C# - benchmark scan of 628 ABFs (over network) took 11.16 seconds
+                    //AbfSharp.ABF abf = new(fullPath);
+
+                    foreach (AbfDatabase database in databases)
+                    {
+                        database.AddAbf(
+                            path: fullPath,
+                            episodes: abf.Header.lActualEpisodes,
+                            date: abf.Header.uFileStartDate,
+                            time: abf.Header.uFileStartTimeMS,
+                            stopwatch: abf.Header.lStopwatchTime
+                        );
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"EXCEPTION: see log.txt for details");
+                    log.WriteLine($"{fullPath}\n{ex}\n");
+                    log.Flush();
                 }
             }
+
+            log.WriteLine($"Stored {count} ABFs in {sw.Elapsed}");
         }
     }
 }

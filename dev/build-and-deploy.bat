35cff9d41b66c08b94bd16f6f742b55759c0f87a
Scott W Harden <swharden@gmail.com>
2021-07-02 12:54:58 -0400

    update deployment script

--- dev/build-and-deploy.bat
+++ dev/build-and-deploy.bat
@@ -4,6 +4,6 @@ echo.
 echo Reuilding project using Release mode...
 dotnet publish ..\src\AbfDB --configuration Release --self-contained --runtime win-x86
 
-explorer ..\src\AbfDB\bin\Release\net5.0\win-x86\publish
+xcopy ..\src\AbfDB\bin\Release\net5.0\win-x86\publish "X:\Software\abfdb" /E
 
 pause

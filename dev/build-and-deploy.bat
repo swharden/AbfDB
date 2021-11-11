1d287a591e5ffb162e5be9b23d0feb04159176a9
Scott W Harden <swharden@gmail.com>
2021-11-11 09:26:35 -0500

    delete v1

--- dev/build-and-deploy.bat
+++
@@ -1,9 +0,0 @@
-echo off
-
-echo.
-echo Reuilding project using Release mode...
-dotnet publish ..\src\AbfDB --configuration Release --self-contained --runtime win-x86
-
-xcopy ..\src\AbfDB\bin\Release\net5.0\win-x86\publish "X:\Software\abfdb" /E
-
-pause

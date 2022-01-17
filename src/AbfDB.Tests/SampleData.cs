760b1531a3fd2643baeb1b7daf25aa340fface48
Scott W Harden <swharden@gmail.com>
2022-01-17 12:24:31 -0500

    tests: improve sample data locator

--- src/AbfDB.Tests/SampleData.cs
+++ src/AbfDB.Tests/SampleData.cs
@@ -6,9 +6,29 @@ namespace AbfDB.Tests;
 
 public static class SampleData
 {
+    private static string GetSampleDataPath(TestContext testContext, int maxDepth = 10)
+    {
+        string possibleRepoRootFolder = Path.GetFullPath(testContext.TestRunDirectory);
+
+        for (int i = 0; i < maxDepth; i++)
+        {
+            string dataPath = Path.Combine(possibleRepoRootFolder ?? string.Empty, "dev/SampleData");
+            if (Directory.Exists(dataPath))
+            {
+                return dataPath;
+            }
+            else
+            {
+                possibleRepoRootFolder = Path.GetDirectoryName(possibleRepoRootFolder) ?? string.Empty;
+            }
+        }
+
+        throw new DirectoryNotFoundException();
+    }
+
     public static string[] GetTestAbfs(TestContext testContext)
     {
-        string dataFolder = Path.GetFullPath(Path.Join(testContext.TestRunDirectory, "../../../dev/SampleData"));
+        string dataFolder = GetSampleDataPath(testContext);
         return Directory.GetFiles(dataFolder, "*.abf").ToArray();
     }
 }
